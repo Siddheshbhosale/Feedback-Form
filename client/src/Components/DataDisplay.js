d7c5b7dcb5c427dc76fa40a2c373affae2065a4c
Siddhesh Bhosale <bhosalesiddhesh9@gmail.com>
2023-11-29 16:09:51 +0530

    Added DataDisplay

--- client/src/Components/DataDisplay.js
+++ client/src/Components/DataDisplay.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import { useState, useEffect } from "react";
+
 
 function DataGridComponent() {
-  const data={};
+  const [data,setdata] = useState([]);
   const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'name', headerName: 'Name', width: 150 },
-    { field: 'email', headerName: 'Email', width: 250 },
+    { field: 'name', headerName: 'Name', width: 70 },
+    { field: 'email', headerName: 'Email', width: 70 },
+    { field: 'class', headerName: 'Class', width: 70 },
+    { field: 'courses', headerName: 'Courses', width: 70 },
+    { field: 'stateOfClass', headerName: 'StateOfClass', width: 120 },
+    { field: 'courseContents', headerName: 'CourseContents', width: 120 },
+    { field: 'audioConnectivity', headerName: 'AudioConnectivity', width: 120 },
+    { field: 'lectureStructure', headerName: 'LectureStructure', width: 120 },
+    { field: 'learningMaterials', headerName: 'LearningMaterials', width: 120 },
+    { field: 'Datetime', headerName: 'Datetime', width: 70 },
+    { field: 'currentCourseTopic', headerName: 'CurrentCourseTopic', width: 70 },
+    { field: 'comments', headerName: 'Comments', width: 70 }
   ];
 
+  const getData = async () => {
+    try {
+      const url = 'http://localhost:8080/';
+      const response = await fetch(url, {
+        method: "GET",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+      const dataResponse = await response.json();
+      // let id = 'id';
+
+      var sampledata =[];
+      dataResponse.map((row,index)=>{
+        sampledata.push({...row, id:index+1});
+        // console.log(row)
+      })
+      setdata(sampledata);
+    } catch (error) {
+      console.log("Error in fetch :" + error);
+    }
+
+  }
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div style={{ height: 400, width: '100%' }}>
       <DataGrid rows={data} columns={columns} pageSize={5} />
